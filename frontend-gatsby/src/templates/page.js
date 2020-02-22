619288f8ac31e9f29e14046bc8b0571aa865d23e
carlopisacane <doncarlopisacane@gmail.com>
2020-02-22 22:23:49 +0100

    Create custom page template in Wordpress to render portfolio items

--- frontend-gatsby/src/templates/page.js
+++ frontend-gatsby/src/templates/page.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
-import PortfolioItems from '../components/PortfolioItems'
 
 export default ({ pageContext }) => {
 	return (
 		<Layout>
 			<h1 dangerouslySetInnerHTML={{ __html: pageContext.title }} />
 			<div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
-			<PortfolioItems />
 		</Layout>
 	)
 }
