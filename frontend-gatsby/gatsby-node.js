58a4dd640dc50a923a3bfea85a3c9173467b2a62
carlopisacane <doncarlopisacane@gmail.com>
2020-02-23 17:38:06 +0100

    blog with pagination

--- frontend-gatsby/gatsby-node.js
+++ frontend-gatsby/gatsby-node.js
@@ -80,7 +80,7 @@ exports.createPages = ({ graphql, actions }) => {
 			})
 			// ==== END PAGES ====
 
-			// ==== POSTS (WORDPRESS NATIVE AND ACF) ====
+			// ==== PORTFOLIO ====
 			.then(() => {
 				graphql(`
           {
@@ -121,6 +121,63 @@ exports.createPages = ({ graphql, actions }) => {
 					resolve()
 				})
 			})
-		// ==== END POSTS ====
+			// ==== END PORTFOLIO ====
+			// ==== BLOG POSTS ====
+			.then(() => {
+				graphql(`
+					{
+  					allWordpressPost{
+    					edges{
+      					node{
+									excerpt
+        					wordpress_id
+        					date(formatString: "DD MMM YYYY HH:mm")
+        					title
+									content
+									slug
+      					}
+    					}
+  					}
+					}
+				`).then((result) => {
+					if (result.errors) {
+						console.log(result.errors)
+						reject(result.errors)
+					}
+
+					const posts = result.data.allWordpressPost.edges
+					const postsPerPage = 2
+					const numberOfPages = Math.ceil(posts.length / postsPerPage)
+					const blogPostListTemplate = path.resolve(
+						'./src/templates/blogPostList.js'
+					)
+
+					Array.from({ length: numberOfPages }).forEach((page, index) => {
+						createPage({
+							component: slash(blogPostListTemplate),
+							path: index === 0 ? '/blog' : `/blog/${index + 1}`,
+							context: {
+								posts: posts.slice(
+									index * postsPerPage,
+									index * postsPerPage + postsPerPage
+								),
+								numberOfPages,
+								currentPage: index + 1
+							}
+						})
+					})
+
+					const pageTemplate = path.resolve('./src/templates/page.js')
+					_.each(posts, (post) => {
+						createPage({
+							path: `/post/${post.node.slug}`,
+							component: slash(pageTemplate),
+							context: post.node
+						})
+					})
+
+					resolve()
+				})
+			})
 	})
 }
